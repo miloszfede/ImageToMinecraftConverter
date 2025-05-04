c2b786b242c3798fb0c529779af2047ca7100840
miloszfede <miloszfede@gmail.com>
2025-05-04 18:38:17 +0200

    Schematic class constructor

--- Schematic.cs
+++ Schematic.cs
@@ -6,7 +6,7 @@ namespace JpgToMinecraftConverter
         public required short Width { get; set; }
         public required short Height { get; set; }
         public required short Length { get; set; }
-        public required string Materials { get; set; }
+        public required string Materials = "Alpha";
         public required byte[] Blocks { get; set; }
         public required byte[] Data { get; set; }
     }
