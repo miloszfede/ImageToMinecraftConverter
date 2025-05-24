4f59620f57435e7038e462febd0142bd8881863d
miloszfede <miloszfede@gmail.com>
2025-05-24 12:04:26 +0200

    Started implementic schematicexporter

---
+++ Services/SchematicExporter.cs
@@ -0,0 +1,10 @@
+namespace JpgToMinecraftConverter.Services;
+
+using JpgToMinecraftConverter.Interfaces;
+using JpgToMinecraftConverter.Models;
+using System.Text.Json;
+
+class SchematicExporter
+{
+
+}
